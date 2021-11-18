ac61080d2e6f2de98c9709b3a3949269321720b7
Varjus <75344148+Varjus@users.noreply.github.com>
2021-11-18 20:35:29 -0300

    correcao de nome de pastas rev 2

--- descartezap/asgi.py
+++ descartezap/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'descartezap_beta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'descartezap.settings')
 
 application = get_asgi_application()
--- descartezap/wsgi.py
+++ descartezap/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'descartezap_beta.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'descartezap.settings.base')
 
 application = get_wsgi_application()
