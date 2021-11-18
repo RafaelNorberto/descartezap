fd1792213ff8388b2f496e261d7477c221b3b585
Varjus <75344148+Varjus@users.noreply.github.com>
2021-11-18 01:02:18 -0300

    clone

--- DescarteZap_Beta/settings/base.py
+++ DescarteZap_Beta/settings/base.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIR = {
     os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = (BASE_DIR / "staticfiles")
+}
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
